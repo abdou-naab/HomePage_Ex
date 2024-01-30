f9e42b1d9795799af414008fce648606d9e18b77
abdou <113465542+abdou-naab@users.noreply.github.com>
2024-01-30 09:26:11 +0100

    added images as well as views.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,20 @@
-var express = require("express");
-var router = express.Router();
+require("dotenv").config();
 
+const express = require("express");
+const router = express.Router();
+const { projects } = require("../data");
+const path = require("path");
 router.get("/", function (req, res, next) {
-  res.render("index");
+  res.render("index", {
+    projects,
+    git: process.env.GITHUB,
+    linkedin: process.env.LINKEDIN,
+    email: process.env.EMAIL,
+  });
+});
+router.get("/cv", function (req, res) {
+  const file = path.join(__dirname, "..", "public", "files", "shop.jpg");
+  res.download(file);
 });
 
 module.exports = router;
